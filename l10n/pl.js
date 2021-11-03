6dd5a40be5c83769035114752fe3c068618480c4
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-11-03 22:38:27 +0100

    l10n: Add Polish translation

---
+++ l10n/pl.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "theming_customcss",
+    {
+    "Custom CSS" : "Niestandardowe CSS",
+	"You can specify your own CSS here. Be aware that this might break something after upgrade." : "Tutaj możesz określić swój własny CSS. Pamiętaj, że po aktualizacji może to coś zepsuć.",
+	"Insert your custom CSS here …" : "Wstaw tutaj swój niestandardowy kod CSS…",
+    "Save" : "Zapisz"
+},
+"nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
